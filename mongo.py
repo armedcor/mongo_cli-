8bfea99c8f01c438243ad114b2216c0148b127f8
Cormac Lawlor <armedcor@gmail.com>
2020-01-28 15:48:20 +0000

    Following walk through with new environment variables for student

---
+++ mongo.py
@@ -0,0 +1,27 @@
+import pymongo
+import os
+from os import path
+if path.exists("env.py"):
+    import env
+
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DBS_NAME = "myTestDB"
+COLLECTION_NAME = "myFirstMDB"
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+        
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DBS_NAME][COLLECTION_NAME]
+
+documents = coll.find()
+
+for doc in documents:
+    print(doc)
